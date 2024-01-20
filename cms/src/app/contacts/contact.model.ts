8157e5d9cb2053c7950f9b0d286585ea047180e7
reaganhsmith <reagansmith@byui.edu>
2024-01-20 15:17:01 -0700

    creating recipe book

---
+++ cms/src/app/contacts/contact.model.ts
@@ -0,0 +1,17 @@
+class Contact {
+    constructor(
+      public id: number,
+      public name: string,
+      public email: string,
+      public phone: string,
+      public imageUrl: string,
+      public group: Contact[] = []
+    ) {
+      this.id = id;
+      this.name = name;
+      this.email = email;
+      this.phone = phone;
+      this.imageUrl = imageUrl;
+      this.group = group;
+    }
+  }
